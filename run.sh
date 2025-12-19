f7f43ef3366f9139379db818037c89de1be46dc5
Jason Javandel <jasonjavandel@protonmail.com>
2025-12-19 09:42:00 -0800

    Add gnome platform requirement to run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+flatpak install flathub org.gnome.Platform//49 org.gnome.Sdk//49
 flatpak-builder --user --install --force-clean build-dir com.cherryyeti.PkgHarbor.json
 flatpak run com.cherryyeti.PkgHarbor 
